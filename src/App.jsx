d724220537803826b25a77298b6b1e388fb286e2
Foram <foramthakker.120@gmail.com>
2026-09-19 13:14:19 +0530

    Add Week 1 project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>My First React Project</h1>
+      <p>Welcome to Week 1 of React Development!</p>
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
